bf8563459737e6384235bef3db3ace3da7b9e32a
Niclas Emdelius <niclas@aretera.se>
2017-05-07 09:43:08 +0200

    Monty example as floppies

--- Ponty/modules_src/ImageResize.ts
+++ Ponty/modules_src/ImageResize.ts
@@ -1,17 +1,27 @@
-﻿import * as contentAggregator from "contentaggregator";
+﻿import * as foopipes from "foopipes";
+import ICallContext = foopipes.ICallContext;
+import IResult = foopipes.IResult;
+import ProcessJsonResult = foopipes.ProcessJsonResult;
+import ProcessStreamResult = foopipes.ProcessStreamResult;
+import IStreamEvent = foopipes.IStreamEvent;
+
 var gm = require('gm').subClass({imageMagick: true});
 
-export function ImageResize(result: contentAggregator.IBinaryResultStream, physicalPath: string, context: contentAggregator.INodeContext)
+export function ImageResize(event: IStreamEvent, context: ICallContext): Promise<IResult>
 {		
-    // Invoke the 'gm' NPM module, and have it pipe the resulting image data back
-    let transform = gm(physicalPath)
-        .resize(50, 50);
+  return new Promise<IResult>((resolve, reject) => {
+        const r = new ProcessStreamResult(context);
 
-	var str;
-    if (context.metadata["image_url"].indexOf(".gif")>0)
-        str = transform.stream("png");
-	else
-		str = transform.stream();
+        // Invoke the 'gm' NPM module, and have it pipe the resulting image data back
+        let transform = gm(event.stream,'img.png')
+            .resize(50, 50);
 
-    str.pipe(result.stream);
+        var str;
+        if (context.metadata["image_url"].indexOf(".gif")>0)
+            str = transform.stream("png");
+        else
+            str = transform.stream();
+
+        str.pipe(r.stream).on('finish', ()=>resolve(r));
+    });  
 }
