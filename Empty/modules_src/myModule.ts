e5dd0e6bce06ec0d40d3b3915985f52b3ec60760
Niclas Emdelius <niclas@aretera.se>
2017-05-12 11:12:18 +0200

    Empty template

--- Empty/modules_src/myModule.ts
+++ Empty/modules_src/myModule.ts
@@ -1,7 +1,22 @@
-﻿import * as contentaggregator from "contentaggregator";
+﻿import * as foopipes from "foopipes";
+import ICallContext = foopipes.ICallContext;
+import IResult = foopipes.IResult;
+import ProcessJsonResult = foopipes.ProcessJsonResult;
+import ProcessStreamResult = foopipes.ProcessStreamResult;
 
-export function myFunction(callback: (error, res) => void, data: Object, context: contentaggregator.INodeContext)
-{
+export function myFunction(event: any, context: ICallContext): Object {
     console.log("Hello world");
-    callback(null, data);
-};
+    event.myId = make_random_id();
+    return event;
+}
+
+function make_random_id() : string
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
